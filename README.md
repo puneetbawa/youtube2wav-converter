1c3171c71e35351cb9d7cb8175ba493e05f1f49c
Puneet Bawa <bawapuneet994@gmail.com>
2020-08-22 21:39:25 +0530

    Adding code to convert youtube videos into wav
    Lossless Audio conversion from YouTube Videos using youtube-dl and ffmpeg

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# youtube2wav-converter
+#YOUTUBE VIDEO TO WAV CONVERTER
+
+#youtube2wav.py
+The script allows the download for the videos uploaded on YouTube and using ffmpeg the clean wav files are generated.
+
+Check requirements.txt 
+
+	1) install youtube-dl
+	2) install ffmpeg
+
+The script using ffmpeg also allows the conversion into the required sampling frequency (Note: -ar 16000 to be put after the input file)
+
+Usage: python youtube2wav.py link
+Example: python youtube2wav.py https://www.youtube.com/watch?v=ZgIzch1Pqj4
---
+++ youtube2wav.py
@@ -0,0 +1,34 @@
+######Cloned with changes from other github profile###########
+############Updated By: Puneet Bawa##############################
+
+from os import walk
+import os
+import sys
+try:
+        link = sys.argv[1]
+except IndexError:
+        scriptName = sys.argv[0]
+        print "Usage: python " + scriptName + " linkOfVideo"
+        exit()
+#Change this path with yours.
+#Also make sure that youtube-dl and ffmpeg installed.
+#Previous versions of youtube-dl can be slow for downloading audio. Make sure you have downloaded the latest version from webpage.
+#https://github.com/rg3/youtube-dl
+mypath = "/home/puneet/research/audios_conv_youtube"
+os.chdir(mypath)
+os.system("youtube-dl --extract-audio " + link)
+
+vidID= link.split("=")[1]
+print "VidID = " + vidID
+f = []
+for (dirpath, dirnames, filenames) in walk(mypath):
+    f.extend(filenames)
+    break
+for i in range(0, len(f)):
+        if ".opus" in f[i] and vidID in f[i]:
+                vidName = f[i]
+                print vidName
+                cmdstr = "ffmpeg -i \"" + vidName + "\" -f wav -ar 16000 -flags bitexact \"" + vidName[:-5] + ".wav"  + "\""
+                print cmdstr
+                os.system(cmdstr)
+                os.remove(vidName) #Will remove original opus file. Comment it if you want to keep that file.
